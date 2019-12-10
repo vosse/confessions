c06286e7087f299b69a5043a2f8e68d1966be3e9
voss-e <jovicevicdev@gmail.com>
2019-12-11 00:31:09 +0100

    Updates

---
+++ precache-manifest.143a71c28a64dd35a50eb6270f6e4dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16ef73e67b67adb534556c9ece6f758f",
+    "url": "/confessions/index.html"
+  },
+  {
+    "revision": "800abf120c3d6b6efb8c",
+    "url": "/confessions/static/css/main.d41b23eb.chunk.css"
+  },
+  {
+    "revision": "f305bd56389bc3b1030a",
+    "url": "/confessions/static/js/2.a6f09b12.chunk.js"
+  },
+  {
+    "revision": "43058e96117b96b30462ad10c6d74bd7",
+    "url": "/confessions/static/js/2.a6f09b12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "800abf120c3d6b6efb8c",
+    "url": "/confessions/static/js/main.d4c0e35b.chunk.js"
+  },
+  {
+    "revision": "8ebc7068461bd54ed934",
+    "url": "/confessions/static/js/runtime-main.5ac8b49a.js"
+  }
+]);
